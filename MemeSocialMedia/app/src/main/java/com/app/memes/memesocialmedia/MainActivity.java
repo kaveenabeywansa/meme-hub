dd5c481875e0505bb0a1b920d2e04184132e078c
Kaveen Abeywansa <kaveen.abeywansa@infor.com>
2023-09-16 23:34:23 +0530

    initial setup

---
+++ MemeSocialMedia/app/src/main/java/com/app/memes/memesocialmedia/MainActivity.java
@@ -0,0 +1,14 @@
+package com.app.memes.memesocialmedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
